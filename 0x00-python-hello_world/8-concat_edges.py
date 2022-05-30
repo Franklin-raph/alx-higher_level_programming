f889bc99e734e77eec9c3abb39599990280811c8
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-05-30 12:48:51 +0100

    correcting task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,3 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(str[39:67] + str[106:112] + str[0:6])
-print("\n")
