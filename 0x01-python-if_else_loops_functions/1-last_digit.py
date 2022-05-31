5fadba5ebbecd7e9e9133fed656f12ba53e56c24
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-05-31 20:21:21 +0100

    formated code to follow pylance coding style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,13 +4,13 @@ number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 
 if(number < 0):
-    print(f"""Last digit of {number} is -{int(repr(number)[-1])}
-     and is less than 6 and not 0""")
+    print(f"Last digit of {number} is -{int(repr(number)[-1])}"
+     " and is less than 6 and not 0")
 elif(int(repr(number)[-1]) > 5):
-    print(f"""Last digit of {number} is {int(repr(number)[-1])}
-     and is greater than 5""")
+    print(f"Last digit of {number} is {int(repr(number)[-1])}"
+    " and is greater than 5")
 elif(int(repr(number)[-1]) == 0):
     print(f"Last digit of {number} is {int(repr(number)[-1])} and is 0")
 elif(int(repr(number)[-1]) < 6):
-    print(f"""Last digit of {number} is {int(repr(number)[-1])} 
-    and is less than 6 and not 0""")
+    print(f"Last digit of {number} is {int(repr(number)[-1])}"
+    " and is less than 6 and not 0")
