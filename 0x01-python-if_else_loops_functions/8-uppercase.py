cc25672c3c0186b6e1776b7e6769c803f512159d
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-05-31 23:50:20 +0100

    task to print all letters in uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     new_string=""
     for i in str:
-        if ord(i) >=97 and ord(i)< 123:
-            new_string+=chr(ord(i)-32)
+        if ord(i) >= 97 and ord(i) < 123:
+            new_string+=chr(ord(i) - 32)
         else:
             new_string+=i
     print("{}".format(new_string))
