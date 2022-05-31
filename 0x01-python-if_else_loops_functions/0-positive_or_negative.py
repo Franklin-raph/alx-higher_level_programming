e8f73b96fe608167bf3fcfa34dca24704782392b
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-05-31 19:49:37 +0100

    worked on control structures

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+
+import random
+number = random.randint(-10, 10)
+# YOUR CODE HERE
+
+if (number < 0):
+    print(f"{number} is negative")
+elif (number > 0):
+    print(f"{number} is positive")
+else:
+    print(f"{number} is zero")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+Task on control structure and loops
