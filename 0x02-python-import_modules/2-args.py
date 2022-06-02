9a29c5b8e576f6d4cf2a615a51b10c7fcabce12d
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-06-02 17:16:33 +0100

    dynamic script code

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+argv = "Hello Welcome To The Best School".split()
+
+if(len(argv) == 0):
+    print(f"{len(argv)} arguements.")
+if(len(argv) == 1):
+    print(f"{len(argv)} arguement.")
+    for i in argv:
+        print("{}: {}".format(argv.index(i) + 1, i))
+elif(len(argv) > 0):
+    print(f"{len(argv)} arguements.")
+    for i in argv:
+        print("{}: {}".format(argv.index(i) + 1, i))
