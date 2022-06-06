819fe7c514fd0669f9116bb681d2af3c630d9051
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-06-06 20:04:01 +0100

    reversing the items in a list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         return None
     length = len(my_list) - 1
     for i in range(len(my_list)):
-        print("{}".format(my_list[length - i]))
+        print("{:d}".format(my_list[length - i]))
