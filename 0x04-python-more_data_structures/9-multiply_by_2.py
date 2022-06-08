cdce58304055a68d910772600cb675de59fcc7b9
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-06-08 23:25:41 +0100

    printing values of a list multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dict = {}
     dictionary_values = a_dictionary.copy()
     for i in dictionary_values:
-        dictionary_values[i] = dictionary_values[i] * 2
-    return dict(sorted(dictionary_values.items()))
+        new_dict[i] = dictionary_values[i] * 2
+    return dict(sorted(new_dict.items()))
--- 0x04-python-more_data_structures/test.py
+++ 0x04-python-more_data_structures/test.py
@@ -1,27 +1,41 @@
-def only_diff_elements(set_1, set_2):
-    new_list = []
-    for i in set_2:
-        for j in set_1:
-            k = i + j
-            new_list.append(k)
-    return new_list
+# def only_diff_elements(set_1, set_2):
+#     new_list = []
+#     for i in set_2:
+#         for j in set_1:
+#             k = i + j
+#             new_list.append(k)
+#     return new_list
 
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-print(only_diff_elements(set_1, set_2))
+# set_1 = { "Python", "C", "Javascript" }
+# set_2 = { "Bash", "C", "Ruby", "Perl" }
+# print(only_diff_elements(set_1, set_2))
 
 
-def only_diff_elements(set_1, set_2):
-    new_list = []
-    for i in set_2:
-        for j in set_1:
-            if i == j:
-                pass
-            else:
-                k = i + j
-                new_list.append(k)
-    return new_list
+# def only_diff_elements(set_1, set_2):
+#     new_list = []
+#     for i in set_2:
+#         for j in set_1:
+#             if i == j:
+#                 pass
+#             else:
+#                 k = i + j
+#                 new_list.append(k)
+#     return new_list
 
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-print(only_diff_elements(set_1, set_2))
+# set_1 = { "Python", "C", "Javascript" }
+# set_2 = { "Bash", "C", "Ruby", "Perl" }
+# print(only_diff_elements(set_1, set_2))
+
+
+def multiply_by_2(a_dictionary):
+    new_dict = {}
+    dictionary_values = a_dictionary.copy()
+    for i in dictionary_values:
+        new_dict[i] = dictionary_values[i] * 2
+    return dict(sorted(new_dict.items()))
+
+a_dictionary = {'John': 12, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
+new_dict = multiply_by_2(a_dictionary)
+print(a_dictionary)
+print("--")
+print(new_dict)
