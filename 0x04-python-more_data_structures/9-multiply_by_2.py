50b63cf92ee2ab3d69369971eab9ff84de02b837
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-06-08 23:20:15 +0100

    printing values of a list multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     dictionary_values = a_dictionary.copy()
-    new_ditionary = {}
     for i in dictionary_values:
-        new_ditionary[i] = dictionary_values[i] * 2
-    return new_ditionary
-
-a_dictionary = {'John': 2, 'Alex': 8, 'Bob': 14, 'Mike': 14, 'Molly': 16}
-print(multiply_by_2(a_dictionary))
+        dictionary_values[i] = dictionary_values[i] * 2
+    return dict(sorted(dictionary_values.items()))
