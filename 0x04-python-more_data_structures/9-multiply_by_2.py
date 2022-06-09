8b7d429c1b7a79838548281b40eef1234947f8fc
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-06-09 06:10:21 +0100

    multiply value by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = {}
-    dictionary_values = a_dictionary.copy()
-    for i in dictionary_values:
-        new_dict[i] = dictionary_values[i] * 2
-    return dict(sorted(new_dict.items()))
+    cpy = a_dictionary.copy()
+    for i in cpy:
+        cpy[i] = cpy[i] * 2
+    return cpy
