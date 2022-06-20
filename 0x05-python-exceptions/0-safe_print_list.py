58d74cf9e92070b5dcb578850df01e27e5266edb
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-06-20 20:38:05 +0100

    satrting out with error handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,6 +3,6 @@ def safe_print_list(my_list=[], x=0):
     try:
         ans = my_list[:x]
         for val in ans:
-            print(val,end="")
+            print("{}".format(val), end="")
     except:
         print("An error occured")
