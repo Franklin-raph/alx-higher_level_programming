ef2560bce7afcfcd5f959cd752acb9593f7a44d0
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-06-20 21:59:57 +0100

    safe printing division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    res = 0
+    result = 0
     try:
-        res = a / b
+        result = a / b
     except:
-        res = None
+        result = None
     finally:
-        print("Inside result: {}".format(res))
-    return res
+        print("Inside result: {}".format(result))
+    return result
