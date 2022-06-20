a3338dba482fd09bfa6002f37e03790f65989282
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-06-20 20:47:10 +0100

    satrting out with error handling

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(val), end="")
             counter += 1
-        except:
+        except IndexError:
             print("An error occured")
     print()
     return counter
