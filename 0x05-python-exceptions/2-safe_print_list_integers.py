e68933fe37859fab92d84b33f300858c62567acf
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-06-20 21:30:13 +0100

    safe printing integer

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    ans = my_list[:x]
+    for val in ans:
+        try:
+            print("{}".format(val), end="")
+            counter += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return counter
