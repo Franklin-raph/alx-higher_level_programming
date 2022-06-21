16e595dbe268f42f56982ba7bdc1471fc8cf0abd
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-06-21 17:16:21 +0100

    added doc strings

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python3
+
 class Square(object):
-    """Doc Strings"""
-    pass
+        """The summary line for a class docstring should fit on one line.
+    Attributes:
+        object (any): Description of `attribute`.
+    """
+        pass
