d3aec9daadac50e562144baa69ad972014748a15
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-06-21 22:24:05 +0100

    square class task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""This is a module with a private attribute"""
+"""This is a module with a private attribute __soze"""
 
 
 class Square:
-    __size = None
     """An empty class with a private attributes."""
-    pass
+    def __init__(self, size):
+        self.__size = size
