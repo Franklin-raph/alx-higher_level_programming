5a4f9718151368e0969b9d43b4feff5d2b04540b
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-06-28 00:08:27 +0100

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is a module with a private attribute __size"""
+
+
+class Rectangle:
+    """An empty class with a private attributes."""
+    def __init__(self, size):
+        self.__size = size
