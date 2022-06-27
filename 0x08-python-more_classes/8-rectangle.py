324e56135ebf68fb407923b8408f4119a03e8e24
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-06-28 00:57:24 +0100

    task 8

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""This module contains an empty Rectangle class"""
+
+
+class Rectangle:
+    """An empty rectangle class with no attributes"""
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    def __str__(self):
+        """string representation of the rectangle with #"""
+        strn = ''
+        for i in range(self.__height):
+            for j in range(self.__width):
+                strn += f"{self.print_symbol}"
+            if i < self.__height - 1 and self.__width != 0:
+                strn += '\n'
+        return strn
+
+    def __repr__(self):
+        """string representation for a formal object creating"""
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        """Called when an instance is deleted"""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """returns the biggest rectangle based on the area"""
+        if type(rect_1) != Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) != Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() < rect_2.area():
+            return rect_2
+        return rect_1
+
+    def area(self):
+        """return the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """return the perimeter of the rectangle"""
+        w = self.__width
+        h = self.__height
+        if w == 0 or h == 0:
+            return 0
+        return (h + w) * 2
+
+    @property
+    def width(self):
+        """Getter for the attribute __width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for the attribute __width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for the attribute __height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for the attribute __height"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
