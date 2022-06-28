ae7eef19e164a7af267ef6bd019891843c047c2b
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-06-28 14:20:13 +0100

    task 18

--- 0x09-python-everything_is_object/text.py
+++ 0x09-python-everything_is_object/text.py
@@ -2,14 +2,23 @@ s1 = [1,2,3]
 s2 = s1
 # print(s1 is s2)
 
+# l1 = [1, 2, 3]
+# l2 = l1
+# l1 = l1 + [4]
+# print(l2)
+
+# def increment(n):
+#     n.append(4)
+
+# l = [1, 2, 3]
+# print(l)
+# increment(l)
+
+
+def assign_value(n, v):
+    n = v
+
 l1 = [1, 2, 3]
-l2 = l1
-l1 = l1 + [4]
-print(l2)
-
-def increment(n):
-    n.append(4)
-
-l = [1, 2, 3]
-print(l)
-increment(l)
+l2 = [4, 5, 6]
+assign_value(l1, l2)
+print(l1)
