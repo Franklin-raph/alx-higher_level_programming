d97a61fcd607dc7b6d4b6f998d027148443e992a
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-07-06 05:08:30 +0100

    returning a json srting

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+import json
 """Module for returning a json string"""
 
 
-import json
 def to_json_string(my_obj):
     """method for returning a json string"""
     return json.dumps(my_obj)
