2a194f0b2338054359848b4584e50734772c27e7
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-07-06 05:35:26 +0100

    json serializing

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Module for returning a json string"""
+"""Module for Serializing json object"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
     """Serializing json"""
-    json_object = json.dumps(my_obj, indent=1)
     with open(filename, 'w', encoding='UTF-8') as outfile:
-        outfile.write(json_object)
+        json.dump(my_obj, outfile)
